26f049eee39c2f1fb7e62d6d76b7a643a4974016
Roopchand reddy <rrvanga@wisc.edu>
2025-05-29 19:05:15 -0500

    testing

--- agents/summarizer_agent.py
+++ agents/summarizer_agent.py
@@ -9,7 +9,8 @@ hf_logging.set_verbosity_error()
 
 model_name = "google/pegasus-xsum"
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
-model = PegasusForConditionalGeneration.from_pretrained(model_name)
+#model = PegasusForConditionalGeneration.from_pretrained(model_name)
+model=None
 
 def summarize_text(text: str, min_length: int = 20, max_length: int = 60) -> str:
     tokens = tokenizer(text, truncation=True, padding="longest", return_tensors="pt")
